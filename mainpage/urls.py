a4ca99185b5507c55bb65b0a6bc6f603a554b6c2
rattyrat00 <dakyoung2451@gmail.com>
2025-07-19 20:21:20 +0900

    a1

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'mainpage'
 
 urlpatterns = [
-    #path('', views.movie_list, name='movie_list'),
-    path('', movie_list, name='movie_list'),
+    path('', views.mainpage_view, name='mainpage'),
+    path('movies/', movie_list, name='movie_list'),
     path('init_db/', views.init_db, name='init_db'),
 ]
 
--- mainpage/views.py
+++ mainpage/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
+def mainpage_view(request):
+    return render(request, 'mainpage.html')
 
 # API에서 영화 데이터 가져오기
 def fetch_movies_from_api():
@@ -22,52 +24,6 @@ def init_db(request):
     return HttpResponse("DB 초기화 완료!")
 
 
-'''
-from django.http import JsonResponse
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
-from .models import Movie
-'''
-'''
-
-def movie_list(request):
-    query = request.GET.get('q', '')  # 검색어 기본값 빈 문자열
-    page = request.GET.get('page', 1)  # 기본값 1페이지
-
-    # 검색어 포함된 영화만 필터링
-    movies = Movie.objects.filter(title_kor__icontains=query)
-
-    # 페이지네이션 설정
-    paginator = Paginator(movies, 12)
-
-    try:
-        page_obj = paginator.page(page)
-    except PageNotAnInteger:
-        page_obj = paginator.page(1)
-    except EmptyPage:
-        page_obj = paginator.page(paginator.num_pages)
-
-    data = [
-        {
-            "title_kor": movie.title_kor,
-            "poster_url": movie.poster_url
-        }
-        for movie in page_obj
-    ]
-
-    # 페이지 정보 포함
-    response = {
-        "movies": data,
-        "page_info": {
-            "current_page": page_obj.number,
-            "total_pages": paginator.num_pages,
-            "has_previous": page_obj.has_previous(),
-            "has_next": page_obj.has_next(),
-        },
-        "query": query  # 현재 검색어도 응답에 포함
-    }
-
-    return JsonResponse(response)
-    '''
 from django.http import JsonResponse
 from .models import Movie
 
@@ -87,4 +43,6 @@ def movie_list(request):
     }
 
     return JsonResponse(response)
+
+
 
