1b47a4e11d413f655fd6bb8182ba28f06af00355
rattyrat00 <dakyoung2451@gmail.com>
2025-07-20 20:20:05 +0900

    edit 20

--- mini/settings.py
+++ mini/settings.py
@@ -152,7 +152,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # 예: 프로젝트 루트에 static 폴더 만들기
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
